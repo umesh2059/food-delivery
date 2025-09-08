ac027d46d5d9175498e521fe969619768b3c6ac8
Umesh gupta <umeshg2059@gmail.com>
2025-09-08 18:37:55 +0530

    adding a hero section

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import './Home.css';
 import Header from '../../components/header/header';
+import ExploreMenu from '../../components/ExploreMenu/ExploreMenu';
 
 
 const Home = () => {
   return (
     <div>
       <Header/>
+      <ExploreMenu/>
     </div>
   )
 }
