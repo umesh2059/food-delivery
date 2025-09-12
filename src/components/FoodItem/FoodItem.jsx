38633928388fe7de73128f1ccad00e9831bd276e
Umesh gupta <umeshg2059@gmail.com>
2025-09-12 21:14:11 +0530

    adding some features

--- src/components/FoodItem/FoodItem.jsx
+++ src/components/FoodItem/FoodItem.jsx
@@ -8,7 +8,7 @@ const FoodItem = ({id,name,price,description,image}) => {
          <img className='food-item-image' src={image} alt=""/>
             </div>      
         <div className="food-item-info">
-            <div className="food-item-name-rating">
+            <div className="food-item-name-ratings">
                 <p>{image}</p>
                 <img src={assets.rating_starts} alt=""/>
             </div>
