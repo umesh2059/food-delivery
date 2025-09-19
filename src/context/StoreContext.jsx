fb05586646fbde41636f6b1725e5a10f5d6eaeea
umesh gupta <umeshg2059@gmail.com>
2025-09-19 13:58:20 +0530

    sdfk

--- src/context/StoreContext.jsx
+++ src/context/StoreContext.jsx
@@ -1,8 +1,19 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 import { food_list } from "../assets/assets";
 export const StoreContext = createContext(null);
 
   const StoreContextProvider =(props)=>{
+
+   const[cartItems,setCartItem]=useState({});
+
+      const addToCart = (cartId)=>{
+        if(!cartItems[itemId]){
+          setCartItem((prev)=>({...prev,[itemId]:1}))
+        }
+      }
+    
+
+
 
    const contextValue={
         food_list
