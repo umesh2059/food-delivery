d763af430dce06ba7a46e3014218d0fa92eb1c57
umesh gupta <umeshg2059@gmail.com>
2025-09-21 18:38:12 +0530

    asdfgj

--- src/components/foodDisplay/foodDisplay.jsx
+++ src/components/foodDisplay/foodDisplay.jsx
@@ -11,8 +11,11 @@ const FoodDisplay = ({category}) => {
         <h2>Top dishes Near you </h2>
         <div className="food-display-list">
             {food_list.map((item,index)=>{
-                return <FoodItem key={index} id={item.id}name={item.name}description={item.description} price={item.price} image={item.image}/>
+              {console.log(category,item.category);}
 
+              if(category==="All"|| category===item.category){
+                return <FoodItem key={index} id={item._id}name={item.name}description={item.description} price={item.price} image={item.image}/>
+              }
             })}
         </div>
       
