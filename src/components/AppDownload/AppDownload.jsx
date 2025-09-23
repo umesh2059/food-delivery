47cdd3a7ef0fe76b5a2f764fcd6e899596fd69b1
umesh gupta <umeshg2059@gmail.com>
2025-09-23 18:30:52 +0530

    adding ui responsive

--- src/components/AppDownload/AppDownload.jsx
+++ src/components/AppDownload/AppDownload.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import './AppDownload.css'
+import { assets } from '../../assets/assets'
 
 const AppDownload = () => {
   return (
-    <div>
-      
+    <div className='app-download' id='app-download'>
+      <p>For better Experience Download <br/>Tomato App</p>
+        <div className='app-download-platforms'>
+          <img src={assets.play_store} alt="" />
+          <img src={assets.app_store} alt="" />
+          </div>      
     </div>
   )
 }
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,7 +3,7 @@ import './Home.css';
 import Header from '../../components/header/header';
 import ExploreMenu from '../../components/ExploreMenu/ExploreMenu';
 import FoodDisplay from '../../components/FoodDisplay/FoodDisplay';
-
+import AppDownload from '../../components/AppDownload/AppDownload';
 
 const Home = () => {
     const[category,setCategory]= useState('All');
@@ -12,6 +12,7 @@ const Home = () => {
       <Header/>
       <ExploreMenu category={category} setCategory={setCategory}/>
       <FoodDisplay category={category}/>
+      <AppDownload/>
     </div>
   )
 }
